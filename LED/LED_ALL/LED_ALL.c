c8b574de3828279a489fe414d649b52471460dbb
zangmen <zangmenhsu@yahoo.co.jp>
2021-12-21 11:50:27 +0800

    20211221 update

--- LED/LED_ALL/LED_ALL.c
+++ LED/LED_ALL/LED_ALL.c
@@ -19,52 +19,32 @@ int j_ping= 0x7E;
 char led_ping[]={
 	0x7E,0xBD,0xDB,0xE7,0xDB,0xBD,0X7E 
 };
+ 
+//霹靂灯亮法
+char led_pilix1[]={
+	0xFE,0xFD,0xFB,0xF7,0xEF,0xDF,0xBF,0x7F 
+};
+char led_pilix2[]={
+	0xBF,0xDF,0xEF,0xF7,0xFB,0xFD,0xFE 
+};
 
 //接龍灯亮法
-char led_rf[]={
-	0x80,0x81,0x13,0x87,
-	0x8F,0x9F,0XBF,0XFF
-};
-
-char led_r1[]={
-	0x80,0x40,0x20,0x10,
-	0x08,0x04,0x02,0x01
-};
-char led_r2[]={
-	0x81,0x41,0x21,0x11,
-	0x09,0x05,0x03
-};
-char led_r3[]={
-	0x13,0x43,0x23,0x13,
-	0x0B,0x07
-};
-char led_r4[]={
-	0x87,0x47,0x27,0x17,0x0F
-};
-char led_r5[]={
-	0x8F,0x4F,0x2F,0x1F
-};
-char led_r6[]={
-	0x9F,0x5F,0x3F
-};
-char led_r7[]={
-	0xBF,0x7F
-};
-char led_r8=0xFF;
-
-
+char led1[] = {0x7F, 0xBF, 0xDF, 0xEF, 0xF7, 0xFB, 0xFD, 0xFE}; //第1回
+char led2[] = {0x7E, 0xBE, 0xDE, 0xEE, 0xF6, 0xFA, 0xFC}; //第2回
+char led3[] = {0x7C, 0xBC, 0xDC, 0xEC, 0xF4, 0xF8}; //第3回
+char led4[] = {0x78, 0xB8, 0xD8, 0xE8, 0xF0}; //第4回
+char led5[] = {0x70, 0xB0, 0xD0, 0xE0}; //第5回
+char led6[] = {0x60, 0xA0, 0xC0}; //第6回
+char led7[] = {0x40, 0x80}; //第7回
+char led8 = 0x00; //第8回
 
 /*主程式*/
 void main(){
    while(1){
 	 LED_01(run_time);
-	 delay(2000);
 	 LED_ping(run_time);
-	 delay(2000);
 	 LED_pilix3(run_time);
-	 delay(2000);
 	 LED_02(run_time);
-	 delay(2000);
    }
 }
 
@@ -74,128 +54,105 @@ void main(){
 void LED_01(int run){
   int x;
   for(x=0;x<=run;x++){
-	 int i,j;
-	 /*如果key=0,則重置*/
-	 while(1){
-		 /*點亮LED 0~7*/
-		 for(i=0;i<8;i++){
-			 P1= j; //給P1送出控制LED的信號
-			 delay(5000);
-			 j<<= 1; // j => 左旋1位元
-			 j= j+1;
-		  }
-		j= 0xfe; //LED 0= 0xfe
+	   int i,j;
+	   /*點亮LED 0~7*/
+	   for(i=0;i<8;i++){
+			P1= j; //給P1送出控制LED的信號
+			delay(7000);
+			j<<= 1; // j => 左旋1位元
+			j= j+1;
+	    }
+	   j= 0xfe; //LED 0= 0xfe
 	}
   }
-}
+
 
 //LED_ping: 乒乓灯
 void LED_ping(int run){
  int x;
- for(x=0;x<=run;x++){
-     char i,m;
-     while(1){
-	     for(i=0;i<3;i++){
-		     P1= j_ping;
-		     delay(2000);
-		     j_ping=led_ping[i];
-	     }
-      }
+ for(x=0;x<run;x++){
+     char i;
+	/*8顆LED*/
+	for(i=0;i<7;i++){
+		P1= led_ping[i];
+		delay(10000); //delay 10sec
+    }
    }   
 }
 
 //LED_pilix3:運行三回霹靂灯
 void LED_pilix3(int run){
-  int x,j;
-  int key=0;
-  for(x=0;x<=run;x++){
-	 char k,i,m;
+  int x,y,i,a;
+  for(x=0;x<run;x++){
 	 /*亮3回*/
-	 for(k=1;k<=key;k++){
-	    	/*亮法:LED 0~7*/
-		  for(i=0;i<8;i++){
-			 P1= j;
-			 delay(1000);
-			 j<<= 1; // j => 左旋1位元
-			 j= j+1;
-		  }
-		  /*亮法:LED 7~0*/
-		  for(m=0;m<8;m++){
-			 P1= j;
-			 delay(1000);
-			 j>>=1; // j => 右旋1位元
-			 j= j|80;
-		 }
-		 j=0xfe;
-	 }
+	    /*左*/
+	    for(i=0;i<8;i++){
+			for(a=0;a<3;a++){
+				P1 = led_pilix1[i];
+				delay(10000);
+				P1 = 0xFF;
+				delay(10000);
+			}
+		} 
+		/*右*/
+		for(x=0;x< 7;x++){
+			for(y=0;y<3;y++){
+				P1 = led_pilix2[x];
+				delay(10000);
+				P1 = 0xFF;
+				delay(10000);
+			}
+		} 
   }
 }
 
 //LED_02: 左至右移的接龍灯
 void LED_02(int run){
-	int x;
-	for(x=0;x<=run;x++){
-		char r1,r2,r3,r4,r5,r6,r7,r8; //開始值
-	    char r1x,r2x,r3x,r4x,r5x,r6x,r7x,r8x; //指定
-	    while(1){
-		   /*第1回*/
-		   for(r1=0;r1<=7;r1++){
-			 r1x = led_rf[0];
-			 P1= r1x;
-			 delay(1000);
-			 r1x= led_r1[r1];
-		   }
-		   /*第2回*/
-		   for(r2=0;r2<=7;r2++){
-			 r2x = led_rf[1];
-			 P1= r2x;
-			 delay(1000);
-			 r2x= led_r2[r2];
-		   }
-		   /*第3回*/
-		   for(r3=0;r3<=7;r3++){
-			 r3x= led_rf[2];
-			 P1= r3x;
-			 delay(1000);
-			 r3x= led_r3[r3];
-		   }
-		   /*第4回*/
-		   for(r4=0;r4<=7;r4++){
-			 r4x= led_rf[3];
-			 P1= r4x;
-			 delay(1000);
-			 r4x= led_r4[r4];
-		   }
-		   /*第5回*/
-		   for(r5=0;r5<=7;r5++){
-			 r5x= led_rf[4];
-			 P1= r5x;
-			 delay(1000);
-			 r5x= led_r5[r5];
-		   }
-		   /*第6回*/
-		   for(r6=0;r6<=7;r6++){
-			 r6x= led_rf[5];
-			 P1= r6x;
-			 delay(1000);
-			 r6x= led_r6[r6];
-		   }
-		  /*第7回*/
-		  for(r7=0;r7<=7;r7++){
-			 r7x= led_rf[6];
-			 P1= r7x;
-			 delay(1000);
-			 r7x= led_r7[r7];
-		  }
-		  /*第8回*/
-		  for(r8=0;r8<=7;r8++){
-			 r8x= led_rf[7];
-			 P1= r8x;
-			 delay(1000);
-			 r8x= led_r8;
-		  }
-	}
-	}
+ int x;
+ for(x=0;x<run;x++){
+    int a, b, c, d, e, f, g; //迴圈變數
+		/*第1回*/
+	    for(a=0;a<8;a++){
+		   P1 = led1[a]; //指定P1=陣列內的內容
+		   delay(10000); //delay 10sec
+	    }
+	    /*第2回*/
+	    for(b=0;b<7;b++){
+		  P1 = led2[b]; //指定P1=陣列內的內容
+		  delay(10000); //delay 10sec
+	    }
+	    /*第3回*/
+	    for(c=0;c<6;c++){
+		  P1 = led3[c]; //指定P1=陣列內的內容
+		  delay(10000); //delay 10sec
+	    }
+	    /*第4回*/
+	    for(d=0;d<5;d++){
+		  P1 = led4[d]; //指定P1=陣列內的內容
+		  delay(10000); //delay 10sec
+	    }
+	    /*第5回*/
+	    for(e=0;e<4;e++){
+		  P1 = led5[e]; //指定P1=陣列內的內容
+		  delay(10000); //delay 10sec
+	    }
+	    /*第6回*/
+	    for(f=0;f<3;f++){
+		  P1 = led6[f]; //指定P1=陣列內的內容
+		  delay(10000); //delay 10sec
+	    }
+	    /*第7回*/
+	    for(g=0;g<2;g++){
+		  P1 = led7[g]; //指定P1=陣列內的內容
+		  delay(10000); //delay 10sec
+	    }
+	    /*第8回*/
+        P1 = led8; //P1=全亮
+	    delay(10000); //delay 10sec
+	    /*全関*/
+	    P1 = 0xFF; //P1=全関
+	    delay(10000); //delay 10sec
+  }
 }
 
 //delay time
